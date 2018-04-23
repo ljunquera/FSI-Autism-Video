50d75d8152423c9f8fd20c6f03cf3f210851054b
Jim Priestley <japriest@microsoft.com>
2018-04-23 12:45:56 -0400

    Stubbed Video Model Classes

---
+++ Autism-Video-API/Autism-Video-API/Models/PathfinderVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autism_Video_API.Models
+{
+    public class PathfinderVideo
+    {
+        public string PatientID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ Autism-Video-API/Autism-Video-API/Models/PathfinderVideos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autism_Video_API.Models
+{
+    public class PathfinderVideos
+    {
+        List<PathfinderVideo> videos;
+
+        public List<PathfinderVideo> Videos { get; set; }
+
+        public PathfinderVideos (string PatientID, DateTime StartTime, DateTime EndTime)
+        {
+            videos = new List<PathfinderVideo>();
+            
+            //TODO: go get videos
+
+
+        }
+    }
+}
