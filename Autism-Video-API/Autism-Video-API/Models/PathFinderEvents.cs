787f2ab1451e46d3ad769e49ec33b46e15405072
Jim Priestley <japriest@microsoft.com>
2018-04-23 12:30:36 -0400

    Stubbed Event Models

---
+++ Autism-Video-API/Autism-Video-API/Models/PathFinderEvents.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autism_Video_API.Models
+{
+    public class PathFinderEvents
+    {
+        List<PathfinderEvent> events;
+
+        public PathFinderEvents(string PatientID, DateTime StartDate, DateTime EndDate)
+        {
+            events = new List<PathfinderEvent>();
+
+            //TODO: add evemts from DB
+        }
+
+        public PathFinderEvents(string PatientID, DateTime StartDate, DateTime EndDate, string Skill)
+        {
+            events = new List<PathfinderEvent>();
+
+            //TODO: add evemts from DB
+        }
+
+        public PathFinderEvents(string PatientID, DateTime StartDate, DateTime EndDate, string Skill, string Target)
+        {
+            events = new List<PathfinderEvent>();
+
+            //TODO: add evemts from DB
+        }
+
+        public List<PathfinderEvent> Events {get;set;}
+    }
+}
---
+++ Autism-Video-API/Autism-Video-API/Models/PathfinderEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autism_Video_API.Models
+{
+    public class PathfinderEvent
+    {
+        public string PatientID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Skill { get; set; }
+        public string Target { get; set; }
+        public string Result { get; set; }
+        public string Comments { get; set; }
+
+        public void Save()
+        {
+            throw new Exception("Not Implemented");
+        }
+    }
+}
