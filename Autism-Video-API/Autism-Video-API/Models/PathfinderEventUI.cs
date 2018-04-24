e94847d903269b73d51d8f3b5707c7fb6d734497
Jim Priestley <japriest@microsoft.com>
2018-04-24 10:47:54 -0400

    refactored ui controller and models

--- Autism-Video-API/Autism-Video-API/Models/PathfinderEventUI.cs
+++ Autism-Video-API/Autism-Video-API/Models/PathfinderEventUI.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Globalization;
+
 namespace Autism_Video_API.Models
 {
     public class PathfinderEventUI
     {
-        public string Text { get; set; }
-        public string RelativeTime { get; set; }
+        public int OffsetSeconds { get; set; }
+        public string Skill { get; set; }
+        public string Target { get; set; }
+        public string Result { get; set; }
+        public string Comments { get; set; }
 
-        public PathfinderEventUI(string text, string relativeTime)
+        public PathfinderEventUI(PathfinderEvent Pe, DateTime VideoStartTime)
         {
-            this.Text = text;
-            this.RelativeTime = relativeTime;
+            this.OffsetSeconds = (DateTime.ParseExact(Pe.TimeStamp,"yyyyMMddHHmmss", CultureInfo.InvariantCulture) - VideoStartTime).Seconds;
+            this.Skill = Pe.Skill;
+            this.Target = Pe.Target;
+            this.Result = Pe.Result;
+            this.Comments = Pe.Comments;
         }
     }
 }
