cebe04e978b1a5f56d985b94fb6288885abecf24
Siddharth Singh <siddharthdl22@gmail.com>
2018-04-24 09:05:32 -0400

    Created a new model and controller to send video with event and the offset time

---
+++ Autism-Video-API/Autism-Video-API/Models/PathfinderEventUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Autism_Video_API.Models
+{
+    public class PathfinderEventUI
+    {
+        public string Text { get; set; }
+        public string RelativeTime { get; set; }
+
+        public PathfinderEventUI(string text, string relativeTime)
+        {
+            this.Text = text;
+            this.RelativeTime = relativeTime;
+        }
+    }
+}
---
+++ Autism-Video-API/Autism-Video-API/Models/PathfinderVideoUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Autism_Video_API.Models
+{
+    public class PathfinderVideoUI
+    {
+        public string FileName { get; set; }
+        public string StartTime { get; set; }
+        public string Token { get; set; }
+        public List<PathfinderEventUI> events;
+        public List<PathfinderEventUI> Events
+        {
+            get
+            {
+                return this.events;
+            }
+        }
+
+        public PathfinderVideoUI(string fileName, string startTime, string token, List<PathfinderEventUI> events)
+        {
+            this.FileName = fileName;
+            this.StartTime = startTime;
+            this.Token = token;
+            this.events = events;
+        }
+    }
+}
