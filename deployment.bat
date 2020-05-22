59744753e4b07c91a1e1f69178a4ca50763914cb
abhi922 <abhishek.jain2692@gmail.com>
2020-05-21 21:22:29 -0400

    Updated deployment script

--- deployment.bat
+++ deployment.bat
@@ -2,6 +2,7 @@
 echo changing directory...
 echo %DEPLOYMENT_SOURCE%
 echo %DEPLOYMENT_TARGET%
+echo %DEPLOYMENT_TEMP%
 cd %DEPLOYMENT_SOURCE%\player
 echo installing packages...
 npm install && npm run build && xcopy %DEPLOYMENT_SOURCE%\player\dist\player\* %DEPLOYMENT_TARGET% /Y /s /i && del %DEPLOYMENT_TARGET%\hostingstart.html
