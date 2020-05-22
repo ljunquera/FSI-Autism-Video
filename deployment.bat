b44f45eb399df7eabb5fe0ce32f390e57fe0e39a
abhi922 <abhishek.jain2692@gmail.com>
2020-05-21 21:14:25 -0400

    Updated deployment script

--- deployment.bat
+++ deployment.bat
@@ -4,4 +4,4 @@ echo %DEPLOYMENT_SOURCE%
 echo %DEPLOYMENT_TARGET%
 cd %DEPLOYMENT_SOURCE%\player
 echo installing packages...
-npm install && npm run build && xcopy %DEPLOYMENT_SOURCE%\player\dist\player %DEPLOYMENT_TARGET% /Y /s /i
+npm install && npm run build && xcopy %DEPLOYMENT_SOURCE%\player\dist\player\* %DEPLOYMENT_TARGET% /Y /s /i && del %DEPLOYMENT_TARGET%\hostingstart.html
