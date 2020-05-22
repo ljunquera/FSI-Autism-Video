1d18fb1888b50c4361f1ef2817383742003a00cd
abhi922 <abhishek.jain2692@gmail.com>
2020-05-21 22:59:59 -0400

    Updated deployement script.

--- deployment.bat
+++ deployment.bat
@@ -3,6 +3,6 @@ echo changing directory...
 echo %DEPLOYMENT_SOURCE%
 echo %DEPLOYMENT_TARGET%
 echo %DEPLOYMENT_TEMP%
-cd %DEPLOYMENT_SOURCE%\player
+cd %DEPLOYMENT_TEMP%\player
 echo installing packages...
-npm install && npm run build && xcopy %DEPLOYMENT_SOURCE%\player\dist\player\* %DEPLOYMENT_TARGET% /Y /s /i && del %DEPLOYMENT_TARGET%\hostingstart.html
+npm install && npm run build && xcopy %DEPLOYMENT_TEMP%\player\dist\player\* %DEPLOYMENT_TARGET% /Y /s /i && del %DEPLOYMENT_TARGET%\hostingstart.html
